502bad1c564e74a4902ee9b13ded0770953ca038
AssortRock0 <AssortRock0@ASSORTROCK>
2024-01-18 16:46:49 +0900

    중간 저장

---
+++ CPP_DataStruct/Vector/Vector.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+
+class MyVector
+{
+public:
+
+private:
+
+};
+
+int main()
+{
+	{
+		std::vector<int> NewVector = std::vector<int>();
+		
+		for (int i = 0; i < 10; i++)
+		{
+			NewVector.push_back(i);
+		}
+
+		for (int i = 0; i < NewVector.size(); i++)
+		{
+			std::cout << NewVector[i] << " ";
+		}
+	}
+
+}
